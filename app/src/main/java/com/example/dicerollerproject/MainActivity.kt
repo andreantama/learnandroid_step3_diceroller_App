24bfca4eee3f0044a0b073de4103ac3331b2a489
andreantama96 <andreanyogatama1@gmail.com>
2020-04-23 16:10:31 -0700

    (ACTIVITY_MAIN.XML): mengganti view menjadi ImageView. (MAINACTIVITY.KT): membuat sebuah seleksi dengan random angkka sehinggan dapatmemlihi gambar secara random

--- app/src/main/java/com/example/dicerollerproject/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dicerollerproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -20,9 +21,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultDice:TextView = findViewById(R.id.result_dice)
+        val resultDice:ImageView = findViewById(R.id.dice_image)
         val randomNumber = Random.nextInt(6) + 1
-        resultDice.text = randomNumber.toString()
+        val resultImage = when(randomNumber){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        resultDice.setImageResource(resultImage)
 
     }
 }
