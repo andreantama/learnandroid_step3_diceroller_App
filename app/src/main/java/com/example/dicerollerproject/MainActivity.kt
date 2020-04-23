44d2e8aae2dad58f8340321f66f69fd7d44d3db5
andreantama96 <andreanyogatama1@gmail.com>
2020-04-23 15:39:11 -0700

    (MAINACTIVITY.KT): Membuat sebuah lamda function yaitu setOnClickListener untuk menghandle sebuah reaction apa yang terjad apabila button di click. Mmebuat Toast

--- app/src/main/java/com/example/dicerollerproject/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dicerollerproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+         Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
